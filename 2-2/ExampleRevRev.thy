16f1d09f3b5e21f0709757a214a902305c3f947a
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-07-08 23:51:12 +0900

    add rev_rev proof

---
+++ 2-2/ExampleRevRev.thy
@@ -0,0 +1,33 @@
+theory ExampleRevRev
+imports Main
+begin
+
+fun app :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"app Nil ys = ys" |
+"app (Cons x xs) ys = Cons x (app xs ys)"
+
+fun rev :: "'a list \<Rightarrow> 'a list" where
+"rev Nil = Nil" |
+"rev (Cons x xs) = app (rev xs) (Cons x Nil)"
+
+lemma app_Nil2 [simp]: "app xs Nil = xs"
+apply(induction xs)
+apply(auto)
+done
+
+lemma app_assoc [simp]: "app (app xs ys) zs = app xs (app ys zs)"
+apply(induction xs)
+apply(auto)
+done
+
+lemma rev_app [simp]: "rev (app xs ys) = app (rev ys) (rev xs)"
+apply(induction xs)
+apply(auto)
+done
+
+theorem rev_rev [simp]: "rev (rev xs) = xs"
+apply(induction xs)
+apply(auto)
+done
+
+end
