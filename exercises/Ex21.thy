4e90f263d196b80a0ae97e39af382dfa86a688fc
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-07-10 23:01:56 +0900

    Exercise 2.1.

---
+++ exercises/Ex21.thy
@@ -0,0 +1,10 @@
+theory Ex21
+imports Main
+begin
+
+value "1 + (2::nat)"   (* 3::nat *)
+value "1 + (2::int)"   (* 3::int *)
+value "1 - (2::nat)"   (* 0::nat *)
+value "1 - (2::int)"   (* -1::int *)
+
+end
